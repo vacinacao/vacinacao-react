760ea62311e2175a6fac5d3066a1cedfdd3ccba3
Thiago <thiagobazinga@gmail.com>
2019-05-03 12:40:58 -0300

    Create router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeApp from '../HomeFolder/HomeApp/HomeApp';
+import Login from '../UserFolder/Login/Login';
+import Register from '../RegisterFolder/Register/Register';
+import Navbar from '../App/Navbar/Navbar';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-
-      </div>
+    return (<BrowserRouter>
+      <Route path="/" exact component={HomeApp}/>
+      <Route path="/login" exact component={Login}/>
+      <Route path="/register" exact component={Register}/>
+      <Navbar/>
+      </BrowserRouter>
     );
   }
 }
--- src/components/App/Navbar/Navbar.js
+++ src/components/App/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="Navbar">
-        
+      <Link to="/login"><p>Login</p></Link>
+      <Link to="/register"><p>Cadastro</p></Link>
       </div>
     );
   }
