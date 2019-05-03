8c5ce3f71a0760fcbbc58d7e245b4ce4aa5bd935
Thiago <thiagobazinga@gmail.com>
2019-05-03 13:24:09 -0300

    Create navbar desktop version

--- src/components/App/Navbar/Navbar.js
+++ src/components/App/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import mainLogo from '../../../assets/Ministerio_da_Saude-logo.png';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 
@@ -6,8 +7,16 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="Navbar">
-      <Link to="/login"><p>Login</p></Link>
-      <Link to="/register"><p>Cadastro</p></Link>
+        <div className="Logo">
+          <img src={mainLogo}/>
+        </div>
+        <div className="Menu">
+          <Link to="#about"><p>Propósito</p></Link>
+          <Link to="#agevacines"><p>Vacinas</p></Link>
+          <Link to="#map"><p>Onde encontrar</p></Link>
+          <Link to="/login"><p>Login</p></Link>
+          <Link to="/register"><p>Cadastro</p></Link>
+        </div>
       </div>
     );
   }
