a2a80914a56951b08ba48039a86eab345137cccd
caiouehara <caioueharamartins@gmail.com>
2019-05-03 21:42:48 -0300

    Create Register

--- src/components/RegisterFolder/Register/Register.js
+++ src/components/RegisterFolder/Register/Register.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Register.css';
+import FormRegister from '../FormRegister/FormRegister';
+import CheckRegister from '../CheckRegister/CheckRegister';
 
 class Register extends React.Component {
   render() {
     return (
       <div className="Register">
+        <FormRegister
         
+        />
+        <CheckRegister
+        
+        />
       </div>
     );
   }
