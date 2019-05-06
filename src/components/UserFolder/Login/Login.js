882e60626531b9d4d2db9b5559913e37c6c8367f
Luis Gustavo Silva <lgusilva@hotmail.com>
2019-05-06 11:44:22 -0300

    Create Login basic structure

--- src/components/UserFolder/Login/Login.js
+++ src/components/UserFolder/Login/Login.js
@@ -5,7 +5,18 @@ class Login extends React.Component {
   render() {
     return (
       <div className="Login">
-        
+        <h2>Login</h2>
+          <div className='Inputs'>
+            <div className="Cpf">
+              <label htmlFor="Cpf">CPF:</label>
+              <input type="number" name='Cpf' />
+            </div>
+            <div className="BirthDate">
+              <label htmlFor="BirthDate">Data de Nascimento:</label>
+              <input type="date" name='BirthDate' />
+          </div>
+          <button>Logar</button>
+          </div>
       </div>
     );
   }
