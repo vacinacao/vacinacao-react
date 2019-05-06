23c4761a8b984bfc7807954a37f150cf70d7adba
caiouehara <caioueharamartins@gmail.com>
2019-05-06 20:11:40 -0300

    Refactor Valid Logic

--- src/components/RegisterFolder/Register/Register.js
+++ src/components/RegisterFolder/Register/Register.js
@@ -48,51 +48,28 @@ class Register extends React.Component {
     fields.forEach(function (currentValue, element) {
       // console.log(newState.userData);
       if (currentValue.length > 0) {
-        newState.FormRegisterValidate.isValid = true;
       }
       else {
-        newState.FormRegisterValidate.isValid = false;
         newState.FormRegisterValidate.error.push(element);
       }
-      // Erro: o form está aceitando quando só o ultimo input tem valor pela ordem da estrutura do if
-
     });
-
     this.setState(newState);
-    this.SingleValidate();
+    this.singleValidate();
+    // this.singleValidate();
     // console.log('Está valido? ' + newState.FormRegisterValidate.isValid);
     // console.log('Elementos Invalidos: '+ newState.FormRegisterValidate.error);
   }
-  SingleValidate = () => {
-    for (let singleError of this.state.FormRegisterValidate.error) {
-      if (singleError === 0) {
-        console.log('0')
-      }
-      else if (singleError === 1) {
-        console.log('1')
-      }
-      else if (singleError === 2) {
-        console.log('2')
-      }
-      else if (singleError === 3) {
-        console.log('3')
-      }
-      else if (singleError === 4) {
-        console.log('4')
-      }
-      else if (singleError === 5) {
-        console.log('5')
-      }
-      else if (singleError === 6) {
-        console.log('6')
-      }
-      else if (singleError === 7) {
-        console.log('7')
-      }
-      else {
-        console.log('wtf');
-      }
+  singleValidate = () => {
+    let newState = this.state;
+    console.log(this.state.FormRegisterValidate.error);
+    if (newState.FormRegisterValidate.error.length === 0) {
+      newState.FormRegisterValidate.isValid = true;
     }
+    else {
+      newState.FormRegisterValidate.isValid = false;
+      newState.FormRegisterValidate.error = [];
+    }
+    this.setState(newState);
   }
 
   render() {
