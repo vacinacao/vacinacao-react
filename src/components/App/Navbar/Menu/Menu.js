c5ecaa2b589dcab5bd249d8259d9adce3f7fa31a
caiouehara <caioueharamartins@gmail.com>
2019-05-08 23:30:58 -0300

    Add Smooth Scroll

--- src/components/App/Navbar/Menu/Menu.js
+++ src/components/App/Navbar/Menu/Menu.js
@@ -4,10 +4,19 @@ import { Link } from 'react-router-dom';
 
 class Menu extends React.Component {
 
+  smoothScroll = () =>{
+    window.scrollTo({
+        top: 1000, //Posição da tela o qual o scroll é atribuido 
+        behavior: "smooth",
+    });
+}
+
+
   render() {
     return (
       <div className={this.props.setClass}>
-          <a href="#about">Propósito</a>
+          {/* <a href="#about">Propósito</a> */}
+          <a onClick={this.smoothScroll}>Propósito</a> 
           <a href="#agevacines">Vacinas</a>
           <a href="#map">Onde encontrar</a>
           <Link to="/login">Login</Link>
